b59f5d5db4bbc7a1ccb158edcf0bec6bd0842f54
Frank-C0 <fkanaco@unsa.edu.pe>
2024-02-04 19:50:07 -0500

    Add Scene Base

--- Assets/Scripts/ObjectsUtilScripts/Movement.cs
+++ Assets/Scripts/ObjectsUtilScripts/Movement.cs
@@ -6,7 +6,7 @@ public class SetVelocity : MonoBehaviour
 {
     private Rigidbody rb;
     // public GameObject nave;
-    private Vector3 velocity;
+    public Vector3 velocity;
     // public float speedV = 20.0f;
     // Start is called before the first frame update
     void Awake()
